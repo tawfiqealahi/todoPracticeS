8e473604f14f8e34fefc183cd42dddc8c8d313e5
tawfiqealahi <tawfiqealahi99@gmail.com>
2022-01-25 04:25:00 -0600

    2

--- todo.js
+++ todo.js
@@ -16,10 +16,35 @@ input.addEventListener('keypress', function(e) {
 // show  input value
 
  function addToDB(todo){
+    //  i can also distructure the todo object given above       ( const {name, isdone, date} = todo; } )
     //  console.log(todo);
-    const li =`
-    
+    const ul = document.getElementById('itemList');
+    const div = document.createElement('div');
+    div.innerHTML =`
+    <li class="list-group-item mb-2">
+    <div class="row">
+        <div class="col">
+        <span class="">${todo.name} </span>
+            <span class="text-muted">${todo.date}</span>
+        </div>
+        <div class="col-lg-2">
+            <div class="d-flex align-items-center">
+                <button class="fas fa-trash-alt badge bg-danger del-btn btn-sm" data-id="1">
+                    ::
+            </button>
+                <button class="fas fa-check badge bg-primary done-btn btn-sm" data-id="1">
+                    ::
+            </button>
+        </div>
+        </div>
+
+    </div>
+
+</li>
     `
+    ul.appendChild(div);
+    console.log(div);
+    // console.log(li);
  }
 
 
