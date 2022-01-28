ff8907298981d381db34e3e1fb753f9f561668c9
tawfiqealahi <tawfiqealahi99@gmail.com>
2022-01-28 00:48:49 -0600

    11

--- todo.js
+++ todo.js
@@ -1,16 +1,48 @@
 const input = document.querySelector('#itemInput');
+const ul = document.querySelector('#itemList');
+let lists = [];
 
 input.addEventListener('keypress', function (e){
     if(e.key=== 'Enter' ){
         const {value} = input;
-        console.log(value);
-    }
-})
+        // console.log(value);
+        const todo = {
+            _id: lists.length+1,
+            name: value,
+            isComplete: false,
+            date: new Date().toLocaleString()
+}
+    addToDB(todo);
+}});
+// show input value
+function addToDB(todo){
+    const div = document.createElement('div');
+    div.innerHTML = `
+        <li class="list-group-item mb-2">
+        <div class="row">
+            <div class="col">
+            <span class="${todo.isDone ? "done" : "" }">${todo.name} </span>
+                <span class="text-muted">${todo.date}</span>
+            </div>
+            <div class="col-lg-2">
+                <div class="d-flex align-items-center">
+                    <button onclick="test(${todo._id})" class="fas fa-trash-alt badge bg-danger del-btn btn-sm" data-id="${todo._id}">
+                        ::
+                </button>
+                    <button onclick="handleDone(${todo._id})" class="fas fa-check badge bg-primary done-btn btn-sm" data-id="1">
+                        ::
+                </button>
+            </div>
+            </div>
+    
+        </div>
+    
+    </li>
+    `
+    ul.appendChild(div);
+    console.log(div);
 
-
-
-
-
+}
 
 
 
